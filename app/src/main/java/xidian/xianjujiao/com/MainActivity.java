b3a12bf55ace01f5f222a8b3cdc2420c6f3c5f5e
sunxin <sunxin@dmtec.cn>
2017-04-24 00:11:10 +0800

    完成首页展示功能

---
+++ app/src/main/java/xidian/xianjujiao/com/MainActivity.java
@@ -0,0 +1,214 @@
+package xidian.xianjujiao.com;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xidian.xianjujiao.com.adapter.MainFragmentPageAdapter;
+import xidian.xianjujiao.com.fragment.ArticleFragment;
+import xidian.xianjujiao.com.fragment.ForumFragment;
+import xidian.xianjujiao.com.fragment.GameFragment;
+import xidian.xianjujiao.com.fragment.VideoFragment;
+import xidian.xianjujiao.com.utils.SystemBarTintManager;
+import xidian.xianjujiao.com.utils.ToastUtil;
+import xidian.xianjujiao.com.view.TipsToast;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager main_viewPager;
+    private List<Fragment> fragemnts = new ArrayList<>();
+    private MainFragmentPageAdapter adapter;
+    private RadioGroup rgp;
+    private TipsToast tipsToast;
+    //退出时间
+    private long exitTime = 0;
+//    private IFlytekUpdate mUpdManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initWindow();
+        initView();
+        initData();
+        setAdapter();
+        setListener();
+        checkUpdate();
+    }
+
+    private void checkUpdate() {
+        //初始化自动更新对象
+//        mUpdManager = IFlytekUpdate.getInstance(this);
+//        //开启调试模式，默认不开启
+//        mUpdManager.setDebugMode(true);
+//        //开启wifi环境下检测更新，仅对自动更新有效，强制更新则生效
+//        mUpdManager.setParameter(UpdateConstants.EXTRA_WIFIONLY, "true");
+//        //设置通知栏使用应用icon，详情请见示例
+//        mUpdManager.setParameter(UpdateConstants.EXTRA_NOTI_ICON, "true");
+//        //设置更新提示类型，默认为通知栏提示
+//        mUpdManager.setParameter(UpdateConstants.EXTRA_STYLE, UpdateConstants.UPDATE_UI_DIALOG);
+//        // 启动自动更新
+//        mUpdManager.autoUpdate(MainActivity.this, new IFlytekUpdateListener() {
+//            @Override
+//            public void onResult(int errorcode, UpdateInfo result) {
+//                Log.i("---->UPDAT",result.toString());
+//                if (errorcode == UpdateErrorCode.OK && result != null) {
+//                    if (result.getUpdateType() == UpdateType.NoNeed) {
+//                        return;
+//                    }
+//                    mUpdManager.showUpdateInfo(MainActivity.this, result);
+//                } else {
+//                    ToastUtil.showShort(MainActivity.this, "请求更新失败\n更新错误码：" + errorcode);
+//                }
+//            }
+//        });
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        MobclickAgent.onResume(this);
+    }
+
+    //初始化窗体布局
+    private void initWindow() {
+        SystemBarTintManager tintManager;
+        //由于沉浸式状态栏需要在Android4.4.4以上才能使用
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintColor(getResources().getColor(R.color.colorBackground));
+            tintManager.setStatusBarTintEnabled(true);
+        }
+    }
+
+    //初始化控件
+    private void initView() {
+        main_viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        //设置viewpager的预加载页数，viewpager默认只会预加载左右两边的页面数据
+        main_viewPager.setOffscreenPageLimit(4);
+        rgp = (RadioGroup) findViewById(R.id.main_rgp);
+        //设置默认第一个为选中状态
+        RadioButton rb = (RadioButton) rgp.getChildAt(0);
+        rb.setChecked(true);
+    }
+
+    //初始化数据
+    private void initData() {
+        ArticleFragment airticle_fragemnt = new ArticleFragment();
+        ForumFragment forum_Fragment = new ForumFragment();
+        GameFragment game_Fragment = new GameFragment();
+        VideoFragment video_Fragment = new VideoFragment();
+        fragemnts.add(airticle_fragemnt);//文章
+        fragemnts.add(video_Fragment);//视频
+        fragemnts.add(forum_Fragment);//论坛
+        fragemnts.add(game_Fragment);//游戏
+    }
+
+    //设置适配器
+    private void setAdapter() {
+        //实例化适配器
+        adapter = new MainFragmentPageAdapter(getSupportFragmentManager(), fragemnts);
+        //设置适配器
+        main_viewPager.setAdapter(adapter);
+    }
+
+    //设置监听
+    private void setListener() {
+        //viewPager的滑动监听
+        main_viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                //获取当前位置的RadioButton
+                RadioButton rb = (RadioButton) rgp.getChildAt(position);
+                //设置为true
+                rb.setChecked(true);
+            }
+        });
+        //RadioGroup的事件监听
+        rgp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                for (int index = 0; index < rgp.getChildCount(); index++) {
+                    RadioButton rb = (RadioButton) rgp.getChildAt(index);
+                    if (rb.isChecked()) {
+                        main_viewPager.setCurrentItem(index, false);
+                        break;
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * 按两次退出应用
+     *
+     * @param keyCode
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                showTips(R.drawable.tips_smile, "再按一次退出程序");
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        MobclickAgent.onPause(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    /**
+     * 自定义toast
+     *
+     * @param iconResId 图片
+     * @param tips      提示文字
+     */
+    private void showTips(int iconResId, String tips) {
+        if (tipsToast != null) {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+                tipsToast.cancel();
+            }
+        } else {
+            tipsToast = TipsToast.makeText(MainActivity.this.getApplication()
+                    .getBaseContext(), tips, TipsToast.LENGTH_SHORT);
+        }
+        tipsToast.show();
+        tipsToast.setIcon(iconResId);
+        tipsToast.setText(tips);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+        //此处解决有时候出现getActivity（）出现null的情况
+    }
+}
---
+++ app/src/main/java/xidian/xianjujiao/com/adapter/ListViewAdapter.java
@@ -0,0 +1,105 @@
+package xidian.xianjujiao.com.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+import org.xutils.x;
+
+import java.util.List;
+
+import xidian.xianjujiao.com.R;
+import xidian.xianjujiao.com.entity.ChapterListItem;
+import xidian.xianjujiao.com.utils.API;
+import xidian.xianjujiao.com.utils.DateUtils;
+
+/**
+ * Created by xhb on 2016/1/19.
+ * Listview的自定义适配器
+ */
+public class ListViewAdapter extends BaseAdapter {
+    private Context context;
+    private List<ChapterListItem> chapterListItems;
+    private LayoutInflater mLayoutInflater;
+
+    public ListViewAdapter(Context context, List<ChapterListItem> chapterListItems) {
+        this.context = context;
+        this.chapterListItems = chapterListItems;
+        if (mLayoutInflater == null) {
+            mLayoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return chapterListItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return chapterListItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.listview_item_layout, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.title = (TextView) convertView.findViewById(R.id.title);
+            viewHolder.date = (TextView) convertView.findViewById(R.id.date);
+            viewHolder.comment = (TextView) convertView.findViewById(R.id.comment);
+            viewHolder.tv_id = (TextView) convertView.findViewById(R.id.tv_id);
+            viewHolder.tv_typeId = (TextView) convertView.findViewById(R.id.tv_typeid);
+            viewHolder.tv_url = (TextView) convertView.findViewById(R.id.tv_url);
+            viewHolder.iv = (ImageView) convertView.findViewById(R.id.iv);
+            //设置tag
+            convertView.setTag(viewHolder);
+        } else {
+            //获取缓存布局
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        ChapterListItem chapterListItem = chapterListItems.get(position);
+        viewHolder.title.setText(chapterListItem.getTitle());
+        //格式化时间
+        String senddate = chapterListItem.getSenddate();
+        String time = DateUtils.dateFormat(senddate);
+        viewHolder.date.setText(time);//文章发布时间
+        viewHolder.comment.setText(chapterListItem.getFeedback());//评论数
+        viewHolder.tv_id.setText(chapterListItem.getId());//文章id
+        viewHolder.tv_typeId.setText(chapterListItem.getTypeid());//文章分类id
+        viewHolder.tv_url.setText(chapterListItem.getArcurl());//文章URl
+        viewHolder.iv.setImageResource(R.drawable.product_default);//设置默认图片
+        final ImageView iv = viewHolder.iv;
+        //获取到图片地址
+        String litpic = chapterListItem.getLitpic();
+        //如果图片地址为空，则设置默认图片
+        if (litpic == null) {
+            iv.setImageResource(R.drawable.product_default);
+        }
+        //地址拼接
+        String imageUrl = API.DMGEAME_URL + litpic;
+        //下载图片，优先使用本地缓存图片
+        x.image().bind(iv,imageUrl);
+        return convertView;
+    }
+
+    //创建一个ViewHolder保存converview的布局
+    class ViewHolder {
+        ImageView iv;//图片
+        //标题、日期、评论数、文章id、分类id、文章地址
+        TextView title, date, comment, tv_id, tv_typeId, tv_url;
+    }
+}
